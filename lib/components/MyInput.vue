e667040e8cfa1b6f1f54ecb08549c4635d5a86fb
zesong <2930048068@qq.com>
2025-10-15 13:46:54 +0800

    添加 MyInput 示例组件

---
+++ lib/components/MyInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { getCurrentInstance, h } from 'vue'
+import type { ComponentInstance } from 'vue'
+import { ElInput } from 'element-plus'
+
+/**
+ * 属性
+ * 事件
+ * 插槽
+ * 方法
+ * 类型
+ */
+
+const vm = getCurrentInstance()
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const changeRef = (exposed: Record<string, any> | null) =>
+  vm && (vm.exposed = exposed)
+
+defineExpose({} as ComponentInstance<typeof ElInput>)
+</script>
+
+<template>
+  <div>
+    <h1>自定义 Input</h1>
+    <component :is="h(ElInput, { ...$attrs, ref: changeRef }, $slots)" />
+  </div>
+</template>
+
+<style scoped lang="css"></style>
